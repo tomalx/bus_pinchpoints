17a5d848f6b050f6d0c9e9c6a831a84487eb0413
TomAlx <33163200+tomalx@users.noreply.github.com>
2024-03-13 17:39:16 +0000

    Update bus_routes_condense.R
    update condense script

--- bus_routes_condense.R
+++ bus_routes_condense.R
@@ -7,7 +7,26 @@
 ### can load data from GIS database using
 ### bus_routes <- st_read(connec,query = "SELECT * FROM arup.existingbusservices") %>%st_transform(crs = 4326)
 
-bus_routes <- st_read(connec,query = "SELECT * FROM arup.existingbusservices") %>%st_transform(crs = 4326)
+bus_routes <- st_read(connec,query = "SELECT * FROM arup.existingbusservices")  %>%st_transform(crs = 4326)
+#open_roads <- st_read(connec,query = "SELECT * FROM os.open_roads_lep")
+bus_corridors <- st_read(connec,query = "SELECT * FROM weca.bus_corridors") %>% st_transform(crs = 4326)
+
+bus_routes <- bus_routes %>% filter(line_name != "FALCON" & freqency > 0)
+
+bus_routes_s <- bus_routes %>% st_snap(open_roads, tolerance = 1)
+bus_routes_s <- bus_routes_s %>% st_transform(crs = 4326)
+
+bus_routes_c <- bus_routes %>% group_by(line_name) %>% summarise() %>% st_intersection() %>% filter(n.overlaps == 1)
+bus_routes_c <- bus_routes_c %>% st_simplify(dTolerance = 1, preserveTopology = TRUE)
+bus_routes_size <- as.numeric(round(object.size(bus_routes)/1024, 0))
+bus_routes_c_size <- as.numeric(round(object.size(bus_routes_c)/1024, 0))
+
+plot(st_geometry(bus_routes_c))
+
+bus_routes_s <- bus_routes_s %>% group_by(line_name) %>% summarise() %>% st_intersection() %>% filter(n.overlaps == 1)
+bus_routes_size <- as.numeric(round(object.size(bus_routes)/1024, 0))
+bus_routes_s_size <- as.numeric(round(object.size(bus_routes_s)/1024, 0))
+bus_corridors_size <- as.numeric(round(object.size(bus_corridors)/1024, 0))
 
 bus_routes <- bus_routes %>% filter(line_name %in% c("48", "48A", "49", "49A"))
 
@@ -61,13 +80,24 @@ bus_routes_touch <- bus_routes %>% group_by(op_code) %>%
 bus_routes_touch_size <- as.numeric(round(object.size(bus_routes_touch)/1024, 0))
 
 
+bus_48_49 <- bus_routes %>% filter(line_name %in% c("48", "48A", "49", "49A")) %>% st_transform(crs = 27700)
+open_roads <- st_read(connec,query = "SELECT * FROM os.open_roads_lep") %>% st_transform(crs = 27700)
 
 
+plot(st_geometry(bus_48_49))
 
+plot(lines)
+plot(simpler_lines)
+plot(road_network, lwd = 5, col = "grey")
+plot(st_geometry(bus_48_49), lwd = 3, col = "grey")
 
+plot(st_snap(bus_48_49, open_roads, tolerance = 0.8), add = TRUE, col = "red")
 
+bus_48_49_snap <- st_snap(bus_48_49, open_roads, tolerance = 1)
+bus_48_49_snap_size <- as.numeric(round(object.size(bus_48_49_snap)/1024, 0))
+bus_48_49_size <- as.numeric(round(object.size(bus_48_49)/1024, 0))
 
-
+bus_48_49_snap <- bus_48_49 %>% group_by(line_name) %>% summarise() %>% st_intersection() %>% filter(n.overlaps == 1)
 
 
 
