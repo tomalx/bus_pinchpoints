5c92c15e58cf612199dca5516b75ad672357f7f4
TomAlx <33163200+tomalx@users.noreply.github.com>
2024-04-27 18:23:22 +0100

    Create load_BODS_gtfs.R

---
+++ load_BODS_gtfs.R
@@ -0,0 +1,9 @@
+## load SW bus open data from DfT
+
+## https://data.bus-data.dft.gov.uk/timetable/download/
+
+## use tidytransit package to load the data
+
+## data downloaded and saved in:
+## C:\Users\tom.alexander1\OneDrive - West Of England Combined Authority\Transport\2.2 Bus\BODS\gtfs
+
