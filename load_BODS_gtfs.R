6b9051547550fa6ea5d84a5260cd9d3ea35bcfaa
TomAlx <33163200+tomalx@users.noreply.github.com>
2024-05-01 09:05:37 +0100

    Update load_BODS_gtfs.R
    missing shapes

--- load_BODS_gtfs.R
+++ load_BODS_gtfs.R
@@ -7,7 +7,52 @@
 ## data downloaded and saved in:
 ## C:\Users\tom.alexander1\OneDrive - West Of England Combined Authority\Transport\2.2 Bus\BODS\gtfs
 
+## script loads gtfs from file then tidies:
+##          - converts to sf
+##          - clips bus routes to only those that travel in the West of England
+##          - add direction_id column in trips table
+##          - identifies trips with missing shapes
+##          - create linestring for missing shapes from stop_times
+
+## load libraries
+library(tidytransit)
+library(sf)
+library(dplyr)
+
+######### load objects from file #########
+# gtfs zip file
 sw_gtfs <- tidytransit::read_gtfs("C:/Users/tom.alexander1/OneDrive - West Of England Combined Authority/Transport/2.2 Bus/BODS/gtfs/itm_south_west_gtfs.zip")
+# weca boundary line
+weca_bdline <- st_read("C:/Users/tom.alexander1/OneDrive - West Of England Combined Authority/Transport/7.0 Data/03 Analysis Projects/accessibility_connectivity/shp/weca_bdline.shp")
+
+######### convert to sf #########
+gtfs <- gtfs_as_sf(sw_gtfs, crs = 27700)
+
+######### clip to weca only services #########
+weca_gtfs <- filter_feed_by_area(gtfs, weca_bdline)
+rm(sw_gtfs, gtfs)
+
+######### add direction_id column in trips table #########
+weca_gtfs$trips <- weca_gtfs$trips %>% left_join(weca_gtfs$routes, by="route_id") %>% 
+  #filter(route_short_name == 7) %>% 
+  group_by(route_id) %>% 
+  mutate(direction_id = as.integer(factor(trip_headsign))) %>% 
+  ungroup()
+
+######### identify trips with missing shapes #########
+# trips with no shape
+trips_missing_shapes <- weca_gtfs$trips %>% filter(shape_id == "") %>% pull(trip_id)
+
+# stop times for trips with missing shapes
+stop_times_missing_shapes <- weca_gtfs$stop_times %>% 
+  filter(trip_id %in% trips_missing_shapes)
+
+
+
+shapes <- weca_gtfs$shapes
+
+
+
 
 ## OP736 = First Bristol, Bath & the West
 first_routes <- sw_gtfs$routes %>% filter(agency_id == "OP736")
