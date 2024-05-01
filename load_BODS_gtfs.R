3d32cafbd279bb826352f9690969322648f8b62f
TomAlx <33163200+tomalx@users.noreply.github.com>
2024-05-01 17:13:48 +0100

    leaflet map and prospective data
    visulaise prospective data

--- load_BODS_gtfs.R
+++ load_BODS_gtfs.R
@@ -49,10 +49,65 @@ stop_times_missing_shapes <- weca_gtfs$stop_times %>%
 
 
 
-shapes <- weca_gtfs$shapes
+shapes <- weca_gtfs$shapes %>% st_transform(4326)
+leaflet() %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  #addProviderTiles(providers$CartoDB.DarkMatter) %>%
+  addPolylines(data = shapes, weight = 2, color = "purple", group = "BOD data" ) %>% 
+  addPolylines(data = bus_routes, weight = 2, color = "cyan", group = "theo DB data") %>% 
+  addLayersControl(baseGroups = c("BOD data", "theo DB data"), options = layersControlOptions(collapsed = FALSE))
+
+route_40s_stops <- weca_gtfs$trips %>% filter(route_short_name %in% c(42,43,44,45)) %>% 
+  left_join(weca_gtfs$stop_times, by="trip_id") %>% 
+  select(stop_id) %>% 
+  distinct() %>% 
+  pull(stop_id)
+
+
+######## connect to DB and load First's Prospective data #########
+
+## IDEA: use a network version of the prospective data to recreate the missing BODS shapes
+# databse connection
+if(!exists("connec")) {
+  # source the connect_postgreSQL.R script
+  source("C:\\Users\\tom.alexander1\\OneDrive - West Of England Combined Authority\\Transport\\7.0 Data\\Rscripts\\access_postgresql\\access_to_postgresql\\connect_postgreSQL.R")
+}
+# sql query (using glue package for syntax formatting)
+sql <- glue_sql( "SELECT * FROM prospective.congestion ",
+                  "WHERE bounding_start_stop IN ({route_40s_stops*}) ",
+                  "AND bounding_end_stop IN ({route_40s_stops*}) ",
+                  "AND data_period = '2023 Oct - 2023 Dec' ",
+                  #"AND hour_of_day = '08:00:00' " ,
+                  #"FETCH FIRST 200 ROWS ONLY", 
+                 .con = connec)
+# fetch data
+route_40s_pros_route <- st_read(connec, query = sql)
 
 
 
+####### tidy prospective data ########
+route_40s_pros_route_tidy <- route_40s_pros_route %>% 
+  group_by(shape, hour_of_day) %>%
+ # first(start_stop_name) %>% 
+  summarise(delay_per_m = mean(delay_per_m), average_total_occupancy = mean(average_total_occupancy)) %>% 
+  st_transform(4326)
+
+####### leaflet plot of prospective data #########
+# palette
+pal <- colorQuantile(palette = "Blues", domain = route_40s_pros_route_tidy$delay_per_m, 6)
+
+
+leaflet() %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  #addProviderTiles(providers$CartoDB.DarkMatter) %>%
+  #addPolylines(data = shapes, weight = 2, color = "purple", group = "BOD data" ) %>% 
+  #addPolylines(data = bus_routes, weight = 2, color = "cyan", group = "theo DB data") %>% 
+  addPolylines(data = route_40s_pros_route_tidy, weight = 4, color = ~pal(route_40s_pros_route_tidy$delay_per_m), group = "prospective data") %>%
+  addLayersControl(baseGroups = c("BOD data", "theo DB data", "prospective data"), options = layersControlOptions(collapsed = FALSE))
+                              
+## next steps                             
+
+# congestion_data_periods <- st_read(connec, query = "SELECT DISTINCT data_period FROM prospective.congestion")
 
 ## OP736 = First Bristol, Bath & the West
 first_routes <- sw_gtfs$routes %>% filter(agency_id == "OP736")
